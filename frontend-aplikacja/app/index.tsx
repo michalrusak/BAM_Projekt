554d9fe46b70f68cddbffbc6608bb3243eeee85e
DAWID598 <81323482+DAWID598@users.noreply.github.com>
2025-01-25 21:41:46 +0100

    better homepage

--- frontend-aplikacja/app/index.tsx
+++ frontend-aplikacja/app/index.tsx
@@ -1,21 +1,51 @@
-import { Text, View, Button } from "react-native";
+import { useEffect, useState } from "react";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
 import { useRouter } from "expo-router";
+import { useAuth } from "../hooks/useAuth";  
 
 export default function Index() {
-  const router = useRouter(); 
+  const router = useRouter();
+  const { getAuthData } = useAuth();
+  const [isLoading, setIsLoading] = useState(true);
 
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Button
-        title="Przejdź do logowania"
-        onPress={() => router.push("/login")} 
-      />
-    </View>
-  );
+  useEffect(() => {
+    const checkAuth = async () => {
+      const { token } = await getAuthData();
+      if (token) {
+        // Jeśli token istnieje
+        router.push("/notes");
+      } else {
+        // Jeśli tokenu nie ma
+        router.push("/login");
+      }
+      setIsLoading(false);
+    };
+
+    checkAuth();
+  }, []);
+
+  if (isLoading) {
+    // Możesz dodać jakąś animację lub ekran ładowania
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>Ładowanie...</Text>
+      </View>
+    );
+  }
+
+  return null; // Strona nie będzie wyświetlana, gdy już przekieruje do loginu lub notes
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#333",
+  },
+});
