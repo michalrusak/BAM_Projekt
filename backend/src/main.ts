b102432f0363460c56ff9d206aed2c4f3ee91fe6
Czechu7 <choocas0@gmail.com>
2025-01-25 19:03:13 +0100

    Refactor HTTPS options to use path.join for certificate file paths and update CORS origin configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,17 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as fs from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
   const httpsOptions = {
-    key: fs.readFileSync('./src/cert/key.pem'),
-    cert: fs.readFileSync('./src/cert/cert.pem'),
+    key: fs.readFileSync(path.join(__dirname, '..', 'src', 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, '..', 'src', 'cert', 'cert.pem')),
   };
 
   const app = await NestFactory.create(AppModule, { httpsOptions });
 
   app.enableCors({
-    origin: ['http://localhost:8081,https://*.ngrok-free.app'],
+    origin: ['http://localhost:8081'],
     methods: 'GET,PUT,POST,DELETE, PATCH',
     allowedHeaders: 'Content-Type, Accept',
     credentials: true,
